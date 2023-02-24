1e185de9fd56a3a08ce63853e9d94cf05fd81b08
apeli23 <apelibrian@gmail.com>
2023-02-24 12:47:18 +0300

    canvas-weight-distributed

---
+++ components/Snap.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import "snapsvg";
+
+class ThanosSnap extends Component {
+  componentDidMount() {
+    // Add the JavaScript code from the tutorial here
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Add the HTML and CSS code from the tutorial here */}
+      </div>
+    );
+  }
+}
+
+export default ThanosSnap;
